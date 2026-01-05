a8346a7f2e41f61dae20d761f27eff90b5819003
TMAdevby <mtipun@mail.ru>
2026-01-05 16:45:35 +0300

    Request

---
+++ src/main/java/RefactoringMultiThreading/Handler.java
@@ -0,0 +1,11 @@
+package RefactoringMultiThreading;
+
+import com.sun.net.httpserver.Request;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
+@FunctionalInterface
+public interface Handler {
+    void handle(Request request, BufferedOutputStream responseStream) throws IOException;
+}
